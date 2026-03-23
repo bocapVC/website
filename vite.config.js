3376c2c0f0977883f7e4b6f5447c7432e817c4d6
BOCAP <bocapbolivia@gmail.com>
2026-03-22 21:07:06 -0400

    Fix Pages base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(() => {
-  const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-  const base = process.env.GITHUB_ACTIONS === 'true' && repositoryName
-    ? `/${repositoryName}/`
-    : '/'
-
-  return {
-    base,
-    plugins: [vue()],
-    assetsInclude: ['**/*.mj']
-  }
+export default defineConfig({
+  base: '/',
+  plugins: [vue()],
+  assetsInclude: ['**/*.mj']
 })
